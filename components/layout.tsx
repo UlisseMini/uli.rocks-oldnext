d5a2f7dc5202c57a77f04d4cd8d1124f1cf72d4c
Ulisse Mini <ulisse.mini@gmail.com>
2020-06-01 08:00:31 -0400

    Big refactor, add metatags etc

---
+++ components/layout.tsx
@@ -0,0 +1,29 @@
+import styles from './layout.module.css'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const title = 'Uli'
+const description = "Ulisse Mini's home on the internet"
+
+export default function Layout ({ children }) {
+  return (
+    <div className={styles.container}>
+
+      <Head>
+        <title>{title}</title>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta
+          name='description'
+          content={description}
+        />
+        <meta name='og:title' content={title} />
+        <meta name='twitter:card' content='summary' />
+      </Head>
+
+      <main className={styles.main}>
+        {children}
+      </main>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Layout from '../components/layout'
 
 const github = 'https://github.com/UlisseMini'
 const gitlab = 'https://gitlab.com/0u'
@@ -8,38 +8,28 @@ const matrix = 'valvate:matrix.org'
 
 export default function Home () {
   return (
-    <>
-      <Head>
-        <title>Uli</title>
-        <link rel='icon' href='/favicon.ico' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      </Head>
+    <Layout>
+      <h1>Hello!</h1>
 
-      <header>
-        <h1>Hello!</h1>
-      </header>
+      <p>
+        My name is Ulisse Mini, I'm a 15 year old software developer
+        with a focus on python, javascript, go and rust.
+      </p>
 
-      <main>
-        <p>
-          My name is Ulisse Mini, I'm a 15 year old software developer
-          with a focus on python, javascript, go and rust.
-        </p>
-
-        <p>
-          If you want to contact me here are a few ways
-        </p>
-        <ol>
-          <li>Matrix: <code>{matrix}</code></li>
-          <li>Discord: <code>{discord}</code></li>
-          <li>
-            Email: <a href={`mailto:${email}`}><code>{email}</code></a>
-          </li>
-        </ol>
-        <p>
-          You can find my projects
-          on <a href={github}>github</a> and <a href={gitlab}>gitlab</a>.
-        </p>
-      </main>
-    </>
+      <p>
+        If you want to contact me here are a few ways
+      </p>
+      <ol>
+        <li>Matrix: <code>{matrix}</code></li>
+        <li>Discord: <code>{discord}</code></li>
+        <li>
+          Email: <a href={`mailto:${email}`}><code>{email}</code></a>
+        </li>
+      </ol>
+      <p>
+        You can find my projects
+        on <a href={github}>github</a> and <a href={gitlab}>gitlab</a>.
+      </p>
+    </Layout>
   )
 }
